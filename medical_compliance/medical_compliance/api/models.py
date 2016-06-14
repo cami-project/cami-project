8fe1a149055b6e11d10cb604aa9017678ad63918
Ionut Prunache <ionut@vitaminsoftware.com>
2016-06-14 20:31:13 +0300

    #7 Add simple endpoint

--- medical_compliance/medical_compliance/api/models.py
+++ medical_compliance/medical_compliance/api/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class MedicationPlan(models.Model):
+    class Meta:
+        db_table = 'medication_plan'
+
+    plan_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=1000)
+    daily_dose = models.IntegerField()
--- medical_compliance/medical_compliance/urls.py
+++ medical_compliance/medical_compliance/urls.py
@@ -17,8 +17,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from tastypie.api import Api
 
+from api.resources import MedicationPlanResource
 
 v1_api = Api(api_name='v1')
+v1_api.register(MedicationPlanResource())
+
 
 urlpatterns = [
     url(r'^api/', include(v1_api.urls)),
