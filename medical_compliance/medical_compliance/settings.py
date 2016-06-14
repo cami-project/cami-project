421273db1d10585a77f36e3c6653409d3946ee36
Ionut Prunache <ionut@vitaminsoftware.com>
2016-06-14 20:29:47 +0300

    #7 Allow CORS

--- medical_compliance/medical_compliance/settings.py
+++ medical_compliance/medical_compliance/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -119,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+X_FRAME_OPTIONS='ALLOW-FROM *'
 
 try:
     from settings_local import *
