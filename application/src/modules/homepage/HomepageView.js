ee898520ceaf744dd3e2e99be6816d0c08a26ad7
rtud <radu.tudorie@rtud.ro>
2016-07-14 15:47:34 +0300

    #24 - Defined base styles for screen layout
    * current styling structure represents an experimental drive to use as few images as possible
    * [WIP]: style will be consolidated as screen dev moves along

--- application/src/modules/homepage/HomepageView.js
+++ application/src/modules/homepage/HomepageView.js
@@ -62,6 +62,99 @@ const HomepageView = React.createClass({
   }
 });
 
-const styles = StyleSheet.create({});
+const buttonCircle = {
+  borderWidth: 0,
+  borderRadius: 45,
+  width: 90,
+  height: 90
+};
+
+const username = 'Jim';
+
+let {height, width} = Dimensions.get('window');
+
+const color = {
+  developing: '#a7b50a'
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: 'moccasin'
+  },
+  iconContainer: {
+    flex: 1,
+    backgroundColor: '#eaeaea',
+    zIndex: 2,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  outerRing: {
+    borderWidth: 2,
+    borderRadius: 72,
+    width: 140,
+    height: 140,
+    borderColor: 'rgba(255,255,255,0.35)',
+    marginBottom: -70,
+    justifyContent: 'center'
+  },
+  iconRing: {
+    borderWidth: 0,
+    borderRadius: 60,
+    width: 120,
+    height: 120,
+    backgroundColor: 'white',
+    alignSelf: 'center',
+    justifyContent: 'center'
+  },
+  mainContainer: {
+    flex: 3,
+    backgroundColor: '#dbdbdb',
+    alignItems: 'center',
+    zIndex: 1
+  },
+  textContainer: {
+    paddingTop: 40,
+    width: width*.8,
+    justifyContent: 'center'
+  },
+  text: {
+    fontSize: 26,
+    color: '#158A12',
+    lineHeight: 1.3*26
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: width*.8,
+    position: 'absolute',
+    bottom: width*.1,
+    left: width*.1
+  },
+  button: {
+    ...buttonCircle,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    shadowRadius: 40,
+    shadowOffset: {width: 0, height: 0},
+    shadowOpacity: 0.35,
+    shadowColor: color.developing
+  },
+  buttonText: {
+    backgroundColor: 'transparent',
+    textAlign: 'center',
+    alignSelf: 'center',
+    fontSize: 22,
+    fontWeight: 'bold'
+  },
+  buttonConfirm: {
+    backgroundColor: 'white',
+  },
+  buttonPanic: {
+    backgroundColor: '#C95F5F'
+  }
+});
 
 export default HomepageView;
