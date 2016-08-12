5cd88528deba89fc916d36bfb7f9f5dbe40c3d79
Ionut Prunache <ionut@vitaminsoftware.com>
2016-08-12 20:28:36 +0300

    #41 Configure DB access

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'frontend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'frontend',
+        'USER': 'cami',
+        'PASSWORD': 'cami',
+        'HOST': 'cami-store',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
