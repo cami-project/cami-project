d1506aa80801b792c33b3e5c635a2545dd3fc1cc
Ionut Prunache <ionut@vitaminsoftware.com>
2016-08-12 21:20:50 +0300

    #41 Add initial data for `frontend` database

--- store/populate_frontend.sql
+++ store/populate_frontend.sql
@@ -1,2 +1,8 @@
 # This will be used to populate the database with initial data after migrations
 # have run.
+
+USE frontend;
+
+INSERT INTO notification (type, severity, message, created) VALUES('blood_pressure', 'high', 'You are close to a stroke!', NOW());
+INSERT INTO notification (type, severity, message, created) VALUES('blood_pressure', 'medium', 'You should be fine soon!', NOW());
+INSERT INTO notification (type, severity, message, created) VALUES('weight', 'low', 'You are alright!', NOW());
