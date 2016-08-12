c1baec4ee71d788b5c285e4c7dac59b26d6909f1
Ionut Prunache <ionut@vitaminsoftware.com>
2016-08-12 20:27:35 +0300

    #41 Import local settings file

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Use local settings file for environment specific configuration
+try:
+    from settings_local import *
+except:
+    pass
