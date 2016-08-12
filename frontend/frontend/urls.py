4f207119469f993e8ab59c8fb68b898d7d778bfb
Ionut Prunache <ionut@vitaminsoftware.com>
2016-08-12 21:20:31 +0300

    #41 Register notification endpoint url

--- frontend/frontend/urls.py
+++ frontend/frontend/urls.py
@@ -15,12 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from tastypie.api import Api
-
-
-v1_api = Api(api_name='v1')
 
 urlpatterns = [
-    url(r'^api/', include(v1_api.urls)),
+    url(r'^api/', include('api.urls')),
     url(r'^admin/', admin.site.urls),
 ]
