77bbfcc98bdb65f714dc9d169fb8ac76a7137c26
Ionut Prunache <ionut@vitaminsoftware.com>
2016-08-15 12:30:15 +0300

    #41 Write Dockerfile for frontend microservice container

---
+++ docker/cami-frontend/docker-entrypoint.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Using hardcoded values for paths for now.
+# TODO(@iprunache): find a way to make the paths configurable
+
+python /cami-project/frontend/manage.py runserver 0.0.0.0:8001
