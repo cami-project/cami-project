66e6fd6808ef4f0d5dacc1288bfdec516db68d7f
Ionut Prunache <ionut@vitaminsoftware.com>
2016-08-15 12:27:32 +0300

    #41 Stop trying to do migrations on the store container
    Found it hard to install Django requirements on the mysql machine so
    gave up on that. Migrations will be run from clones of application
    containers.

--- store/migrate.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-# This will run migrations for all microservices
--- store/populate_frontend.sql
+++
@@ -1,8 +0,0 @@
-# This will be used to populate the database with initial data after migrations
-# have run.
-
-USE frontend;
-
-INSERT INTO notification (type, severity, message, created) VALUES('blood_pressure', 'high', 'You are close to a stroke!', NOW());
-INSERT INTO notification (type, severity, message, created) VALUES('blood_pressure', 'medium', 'You should be fine soon!', NOW());
-INSERT INTO notification (type, severity, message, created) VALUES('weight', 'low', 'You are alright!', NOW());
