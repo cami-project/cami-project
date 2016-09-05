1d13a0b38647695622e93d461013dae9643dc866
Alexandru Sorici <alex.sorici@gmail.com>
2016-09-05 13:46:22 +0300

    Removed old MeasurementNotificationResource entries from url paths

--- medical_compliance/medical_compliance/urls.py
+++ medical_compliance/medical_compliance/urls.py
@@ -17,12 +17,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from tastypie.api import Api
 
-from api.resources import MedicationPlanResource, MeasurementNotificationResource
+from api.resources import MedicationPlanResource
 from api import views
 
 v1_api = Api(api_name='v1')
 v1_api.register(MedicationPlanResource())
-v1_api.register(MeasurementNotificationResource())
 
 urlpatterns = [
     url(r'^api/', include(v1_api.urls)),
