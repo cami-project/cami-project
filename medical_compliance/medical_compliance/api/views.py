afd6438ec7824d83580b32a0f6095e43d36850cc
Ionut Paraschiv <ionut.paraschiv@3angle.tech>
2016-09-21 16:12:32 +0300

    #54 Fixed withings parsing issues & measures are now saved in db

--- medical_compliance/medical_compliance/api/views.py
+++ medical_compliance/medical_compliance/api/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.conf import settings #noqa
 from django.views.decorators.csrf import csrf_exempt
 from withings import WithingsApi, WithingsCredentials
@@ -12,8 +12,8 @@ import json, logging, pprint
 
 logger = logging.getLogger("medical_compliance.measurement_callback")
 
-FULL_CALLBACK_URL = 'http://46.101.163.224:8000/notify_measurements/'
-
+def get_full_callback_url(request):
+    return request.build_absolute_uri("/notify_measurements/")
 
 def subscribe_notifications(request):
     credentials = WithingsCredentials(access_token=settings.WITHINGS_OAUTH_V1_TOKEN,
@@ -22,9 +22,7 @@ def subscribe_notifications(request):
                                       consumer_secret=settings.WITHINGS_CONSUMER_SECRET,
                                       user_id=settings.WITHINGS_USER_ID)
     client = WithingsApi(credentials)
-    #FULL_CALLBACK_URL = request.build_absolute_uri(MeasurementNotificationResource().get_resource_uri())
-
-    response_data = client.subscribe(FULL_CALLBACK_URL, "Subscribe for weight measurement notifications.", appli=1)
+    response_data = client.subscribe(get_full_callback_url(request), "Subscribe for weight measurement notifications.", appli=1)
     return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 def unsubscribe_notifications(request):
@@ -34,20 +32,20 @@ def unsubscribe_notifications(request):
                                       consumer_secret=settings.WITHINGS_CONSUMER_SECRET,
                                       user_id=settings.WITHINGS_USER_ID)
     client = WithingsApi(credentials)
-
-    #FULL_CALLBACK_URL = request.build_absolute_uri(MeasurementNotificationResource().get_resource_uri())
-
-    response_data = client.unsubscribe(FULL_CALLBACK_URL, appli=1)
+    response_data = client.unsubscribe(get_full_callback_url(request), appli=1)
     return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 @csrf_exempt
 def notify_measurements(request):
     logger.debug(pprint.pformat(request.POST))
-    # TODO: make this nicer and more error proof (i.e. check for existance of fields in post data)
-    fetch_measurement.delay(
-        request.POST['userid'],
-        request.POST['startdate'],
-        request.POST['enddate'],
-        request.POST['appli'])
+    
+    if request.POST.has_key("userid"):
+        userid = request.POST['userid']
+        startdate = request.POST['startdate']
+        enddate = request.POST['enddate']
+        appli = request.POST['appli']
+        fetch_measurement.delay(userid, startdate, enddate, appli)
+    else:
+        logger.debug(pprint.pformat("Received invalid post data; possibly related to a subscribe call"))
 
     return HttpResponse(status=200)
