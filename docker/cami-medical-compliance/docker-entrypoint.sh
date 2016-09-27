3dd9c0089bdbb10496e4df416a2d179afe17c2c2
Ionut Paraschiv <ionut.paraschiv@3angle.tech>
2016-09-27 15:06:21 +0300

    #65 Refactored docker files to run the whole project

--- docker/cami-medical-compliance/docker-entrypoint.sh
+++ docker/cami-medical-compliance/docker-entrypoint.sh
@@ -5,6 +5,4 @@
 
 # Doing a migration at the container start is not a good idea on the long term
 # as usually one wants containers to start as fast as possible.
-python /cami-project/medical_compliance/manage.py migrate
-
 python /cami-project/medical_compliance/manage.py runserver 0.0.0.0:8000
