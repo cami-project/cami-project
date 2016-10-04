ccb72448d7e7ecffb00fd6bab50d9bc058a560ef
Alexandru Cretu <cretu.alexandru007@gmail.com>
2016-10-04 13:45:54 +0300

    #75 Added dummy endpoint for healthcheck

---
+++ frontend/api/healthchecker.py
@@ -0,0 +1,5 @@
+class Healthchecker:
+	def __init__(self):
+		self.status = "ok"
+		self.mysql = "ok"
+		self.message_queue = "failed"
--- frontend/api/urls.py
+++ frontend/api/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from tastypie.api import Api
 
-from resources import NotificationResource
+from resources import NotificationResource, HealthcheckResource
 
 v1_api = Api(api_name='v1')
 v1_api.register(NotificationResource())
+v1_api.register(HealthcheckResource())
 
 urlpatterns = [
     url(r'^', include(v1_api.urls)),
