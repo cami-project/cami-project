28a7565e57d1f541dca270c96c5dc0b45b69a5df
rtud <radu.tudorie@rtud.ro>
2016-12-05 17:57:49 +0200

    #118 - Created separate extension to WP functions for enabling overrides
    * we've included a new WP filter that will append classes to body
      * this will insure that we increase the specificity score for
      overrides
    * as it seems that we keep extending the functions.php tpl, I've broken
    the additions into a separate file
      * [WIP] this file will be imported in production via the same deploy procedure
      * [WIP] the production functions.php will be adapted accordingly

---
+++ website/includes/_functions-overrides.php
@@ -0,0 +1,40 @@
+<?php
+  /**
+  * _______ _______  ______  _____  __   _  _____  _     _ _______
+  * |  |  | |_____| |_____/ |     | | \  | |   __| |     | |_____|
+  * |  |  | |     | |    \_ |_____| |  \_| |____\| |_____| |     |
+  *
+  * Theme Name:   cami-web
+  * Author:       rtud
+  * Company:      Maronqua
+  * Company URI:  http://maronqua.ro
+  * File Name:    _functions-overrides.php
+  * Notes:        File needs to be required at the end of theme's main functions.php
+  */
+
+// Styles: Append the global styles overrides
+// -----------------------------------------------------------------
+function mq_global_styles_overrides() {
+  wp_register_style( 'mq-bundle-styles-overrides', get_stylesheet_directory_uri() . '/css/styles-overrides-bundle.min.css', '', date('U', filemtime(get_stylesheet_directory() . '/css/styles-overrides-bundle.min.css')), '');
+  wp_enqueue_style( 'mq-bundle-styles-overrides' );
+}
+add_action('wp_print_styles', 'mq_global_styles_overrides');
+
+
+// Styles: Append the global styles overrides
+// -- classes will be used to provide increase in styles specificity
+// -- this will insure priority of overrides over existing ones
+// -----------------------------------------------------------------
+add_filter('body_class', 'mq_global_body_classes');
+function mq_global_body_classes($classes) {
+
+  $body_classes_overrides = array(
+    'mq',
+    'maronqua',
+    'styles-overrides',
+    'styles-overrides-bundle'
+  );
+
+  return array_merge($classes, $body_classes_overrides);
+}
+
