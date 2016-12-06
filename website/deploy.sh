26a8fa3caf281660b9a86c5bfdb38bff4a31925d
rtud <radu.tudorie@rtud.ro>
2016-12-06 12:49:57 +0200

    #118 - Fixed styles output file name and improved log entry boundry

--- website/deploy.sh
+++ website/deploy.sh
@@ -6,14 +6,14 @@ bold=$(tput bold)
 reset=$(tput sgr0)
 symHammers='\xe2\x9a\x92'
 
-# Grab the latest compiled bundfle from GitHub and add it to the website theme's CSS dir
+# Grab the latest compiled bundle from GitHub and add it to the website theme's CSS dir
 # - this will also output an activity log inside the home dir
 echo -e "\n${symHammers} ${bold}${white}Grabbing latest styles overrides...${reset}\n"
-wget --output-document=/var/www/html/wp-content/themes/theme/css/test.css https://raw.githubusercontent.com/cami-project/cami-project/master/website/assets/styles/styles-overrides-bundle.min.css 2>&1 | tee -a $HOME/deploy.log
+wget --output-document=/var/www/html/wp-content/themes/theme/css/styles-overrides-bundle.min.css https://raw.githubusercontent.com/cami-project/cami-project/master/website/assets/styles/styles-overrides-bundle.min.css 2>&1 | tee -a $HOME/deploy.log
 
 # Grab the latest _functions-overrides.php tpl that enables the overrides capabilities
 echo -e "\n${symHammers} ${bold}${white}Grabbing latest functions overrides...${reset}\n"
 wget --output-document=/var/www/html/wp-content/themes/theme/_functions-overrides.php https://raw.githubusercontent.com/cami-project/cami-project/master/website/assets/includes/_functions-overrides.php 2>&1 | tee -a $HOME/deploy.log
 
 # Insure spacing in-between log entries
-echo -e "\n\n\n" >> deploy.log
+echo -e "\n=============================================================\n" >> deploy.log
