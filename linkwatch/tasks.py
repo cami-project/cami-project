4ffafc6b59fc97ebd0ad9d7e1600bba0d3ad01d6
Dragos Pricope <dragos.pricope@3angle.tech>
2017-01-18 15:53:22 +0200

    #126 Redirect linkwatch log also to stdout

--- linkwatch/tasks.py
+++ linkwatch/tasks.py
@@ -1,10 +1,28 @@
 from celery import Celery
 from kombu import Queue, Exchange
 from kombu.common import Broadcast
-from custom_logging import logger
 
 import endpoints
 from settings import *
+
+import logging
+from logging.handlers import SysLogHandler
+import sys
+
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+stdout = logging.StreamHandler(sys.stdout)
+stdout.setFormatter(formatter)
+
+syslog = SysLogHandler(address=(PAPERTRAILS_LOGGING_HOSTNAME, PAPERTRAILS_LOGGING_PORT))
+syslog.setFormatter(formatter)
+
+root.addHandler(stdout)
+root.addHandler(syslog)
+
 
 app = Celery('api.tasks', broker=BROKER_URL)
 app.conf.update(
@@ -16,5 +34,5 @@ app.conf.update(
 
 @app.task(name=BROKER_TASK)
 def parse_measurement(measurement_json):
-    logger.debug('[linkwatch] Parse measurement request: %s' % (measurement_json))
+    root.debug('[linkwatch] Parse measurement request: %s' % (measurement_json))
     # endpoints.save_measurement(measurement_json)
