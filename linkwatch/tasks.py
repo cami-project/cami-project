9b339136db476430a77e705b79c7cf428d9d07a7
Dragos Pricope <dragos.pricope@3angle.tech>
2017-01-18 16:27:16 +0200

    #126 Changed the way the logger is retrieved in linkwatch

--- linkwatch/tasks.py
+++ linkwatch/tasks.py
@@ -1,4 +1,6 @@
 from celery import Celery
+from celery.utils.log import get_task_logger
+
 from kombu import Queue, Exchange
 from kombu.common import Broadcast
 
@@ -9,8 +11,8 @@ import logging
 from logging.handlers import SysLogHandler
 import sys
 
-root = logging.getLogger()
-root.setLevel(logging.DEBUG)
+logger = get_task_logger('linkwatch')
+logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
@@ -20,8 +22,8 @@ stdout.setFormatter(formatter)
 syslog = SysLogHandler(address=(PAPERTRAILS_LOGGING_HOSTNAME, PAPERTRAILS_LOGGING_PORT))
 syslog.setFormatter(formatter)
 
-root.addHandler(stdout)
-root.addHandler(syslog)
+logger.addHandler(stdout)
+logger.addHandler(syslog)
 
 
 app = Celery('api.tasks', broker=BROKER_URL)
@@ -34,5 +36,5 @@ app.conf.update(
 
 @app.task(name=BROKER_TASK)
 def parse_measurement(measurement_json):
-    root.debug('[linkwatch] Parse measurement request: %s' % (measurement_json))
+    logger.debug('[linkwatch] Parse measurement request: %s' % (measurement_json))
     # endpoints.save_measurement(measurement_json)
