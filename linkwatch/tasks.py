3829c1bfa6f2fa4c6fb02dd8960aaac8e3ce629c
Dragos Pricope <dragos.pricope@3angle.tech>
2017-01-18 17:01:54 +0200

    #126 Cleaned up logging in linkwatch module

--- linkwatch/tasks.py
+++ linkwatch/tasks.py
@@ -5,26 +5,10 @@ from kombu import Queue, Exchange
 from kombu.common import Broadcast
 
 import endpoints
-from settings import *
-
-import logging
-from logging.handlers import SysLogHandler
 import sys
 
-logger = get_task_logger('linkwatch')
-logger.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-stdout = logging.StreamHandler(sys.stdout)
-stdout.setFormatter(formatter)
-
-syslog = SysLogHandler(address=(PAPERTRAILS_LOGGING_HOSTNAME, PAPERTRAILS_LOGGING_PORT))
-syslog.setFormatter(formatter)
-
-logger.addHandler(stdout)
-logger.addHandler(syslog)
-
+from custom_logging import logger
+from settings import *
 
 app = Celery('api.tasks', broker=BROKER_URL)
 app.conf.update(
@@ -37,4 +21,4 @@ app.conf.update(
 @app.task(name=BROKER_TASK)
 def parse_measurement(measurement_json):
     logger.debug('[linkwatch] Parse measurement request: %s' % (measurement_json))
-    # endpoints.save_measurement(measurement_json)
+    endpoints.save_measurement(measurement_json)
