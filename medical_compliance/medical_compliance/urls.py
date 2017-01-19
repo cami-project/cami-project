7b8a8da5d5b9b1121b41a38743c8ff3f00279385
Dragos Pricope <dragos.pricope@3angle.tech>
2017-01-19 14:03:30 +0200

    #124 Renamed Withings hook method & added logging along the Withings subscription flow

--- medical_compliance/medical_compliance/urls.py
+++ medical_compliance/medical_compliance/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^subscribe_notifications/', views.subscribe_notifications, name="subscribe_notifications"),
     url(r'^unsubscribe_notifications/', views.unsubscribe_notifications, name="unsubscribe_notifications"),
-    url(r'^notify_measurements/$', views.notify_measurements, name="notify_measurements"),
+    url(r'^measurements_notification/$', views.measurements_notification_received, name="measurements_notification_received"),
     url(r'^test/$', views.test_heart_rate_fetch, name="test_heart_rate_fetch"),
 ]
