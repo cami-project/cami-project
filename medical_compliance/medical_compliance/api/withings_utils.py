ea9c90c58bee03d86b355b283184826d988c6808
Dragos Pricope <dragos.pricope@3angle.tech>
2017-01-23 13:08:08 +0200

    #127 Added more logging when retrieving user from settings

--- medical_compliance/medical_compliance/api/withings_utils.py
+++ medical_compliance/medical_compliance/api/withings_utils.py
@@ -1,6 +1,9 @@
 import json
+import logging
 
 from django.conf import settings
+
+logger = logging.getLogger("medical_compliance")
 
 class WithingsUser(object):
     def __init__(self, userid, oauth_token, oauth_token_secret):
@@ -10,6 +13,10 @@ class WithingsUser(object):
 
 def get_withings_user(userid):
     result = [user for user in settings.WITHINGS_USERS if user['userid'] == userid]
+
+    logger.debug("[medical-compliance] settings.WITHINGS_USERS: %s" % (settings.WITHINGS_USERS))
+    logger.debug("[medical-compliance] Looked up userid: %s" % (userid))
+    logger.debug("[medical-compliance] Look up result: %s" % (result))
 
     if len(result) == 0:
         raise Exception('Withings user with userid %s could not be found' % (userid))
