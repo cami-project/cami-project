1033d1e12edd5fca6ef8698f36b7fe3754fa4ac8
Alexandru Sorici <alex.sorici@gmail.com>
2017-03-03 16:08:32 +0200

    Migration entrypoint takes care of Django Superuser creation

--- docker/cami-store/docker-migration-entrypoint.sh
+++ docker/cami-store/docker-migration-entrypoint.sh
@@ -2,5 +2,25 @@
 
 # Using hardcoded values for paths for now.
 
+# Run migration first
 python /cami-project/store/manage.py migrate
 python /cami-project/store/manage.py loaddata initialize.yaml
+
+# Create Django superuser account if not exists
+USER="cami"
+PASS="CamiAdmin123$"
+MAIL="proiect.cami@gmail.com"
+script="
+from django.contrib.auth.models import User;
+
+username = '$USER';
+password = '$PASS';
+email = '$MAIL';
+
+if User.objects.filter(username=username).count()==0:
+    User.objects.create_superuser(username, email, password);
+    print('Superuser created.');
+else:
+    print('Superuser creation skipped.');
+"
+printf "$script" | python manage.py shell
