a470f2a675e167a1246b40a890bce906bd9531ef
Alexandru Sorici <alex.sorici@gmail.com>
2017-03-03 11:45:06 +0200

    Adding all model entities to admin view

--- store/store/admin.py
+++ store/store/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from models import UserAccount, Activity, InterfaceService
+from models import UserAccount, Activity, ExternalMonitoringService, Measurement, Caregiver, Device, DeviceUsage
 
-admin.site.register([UserAccount, Activity, InterfaceService])
+admin.site.register([UserAccount, Caregiver, ExternalMonitoringService, Device, DeviceUsage,
+                     Measurement, Activity])
