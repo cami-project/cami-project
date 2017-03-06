e99a8311496dc6ee48fb12dbc1a1c4b67dfe96a0
Alexandru Sorici <alex.sorici@gmail.com>
2017-03-06 13:44:59 +0200

    Altered CAMI Data model as per post-merge comments to pull request #131. Using Django default user model and adding profile data for end-user, caregiver and health professional

--- store/store/admin.py
+++ store/store/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from models import UserAccount, Activity, ExternalMonitoringService, Measurement, Caregiver, Device, DeviceUsage
 
-admin.site.register([UserAccount, Caregiver, ExternalMonitoringService, Device, DeviceUsage,
+from models import EndUserProfile, CaregiverProfile, HealthProfessionalProfile, \
+    Activity, ExternalMonitoringService, Measurement, \
+    Device, DeviceUsage
+
+# admin.site.register(CaregiverProfile, CaregiverAdmin)
+# admin.site.register(HealthProfessionalProfile, HealthProfessionalAdmin)
+
+admin.site.register([EndUserProfile, CaregiverProfile, HealthProfessionalProfile])
+admin.site.register([ExternalMonitoringService, Device, DeviceUsage,
                      Measurement, Activity])
+
--- store/store/models.py
+++ store/store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django_mysql.models import JSONField
+from django.contrib.auth.models import User
+
 from django.utils.translation import ugettext_lazy as _
 from django.core.exceptions import ValidationError
 from django.utils import timezone
@@ -8,19 +10,13 @@ import uuid
 # Create your models here.
 
 # ============ User Information ============
-class PersonalUserInfo(models.Model):
+class UserProfileBase(models.Model):
     class Meta:
         abstract = True
 
     GENDER = (
         ('M', 'Male'),
         ('F', 'Female')
-    )
-    MARITAL_STATUS = (
-        ("single", "single"),
-        ("married", "married"),
-        ("divorced", "divorced"),
-        ("widowed", "widowed")
     )
 
     LANG = (
@@ -30,16 +26,6 @@ class PersonalUserInfo(models.Model):
         ('pl', "Polish")
     )
 
-    gender = models.CharField(max_length=1, choices=GENDER, default='M', null=True, blank = True)
-    marital_status = models.CharField(max_length=16, choices=MARITAL_STATUS, default='married', null=True, blank=True)
-    phone = models.CharField(max_length=16, null = True, blank = True)
-    address = models.CharField(max_length=256, null = True, blank=True)
-    language = models.CharField(max_length=2, choices=LANG, default="en", null = True, blank = True)
-    age = models.PositiveIntegerField(null = True, blank = True)
-    height = models.PositiveIntegerField(null = True, blank=True)
-
-
-class UserAccount(PersonalUserInfo):
     ACCOUNT_ROLES = (
         ('end_user', 'End User'),
         ('caregiver', 'Caregiver'),
@@ -47,41 +33,53 @@ class UserAccount(PersonalUserInfo):
     )
 
     uuid = models.UUIDField(unique=True, default=uuid.uuid4)
-    first_name = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128)
-    email = models.EmailField()
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    verified = models.BooleanField(default=False)
-    verified_date = models.DateField(default=timezone.now)
     valid_from = models.DateField(default=timezone.now)
     valid_to = models.DateField(default=timezone.now)
-    # status = models.CharField
+    updated_at = models.DateTimeField(auto_now=True)
+
     account_role = models.CharField(max_length=16, choices=ACCOUNT_ROLES, default='end_user')
 
+    gender = models.CharField(max_length=1, choices=GENDER, default='M', null=True, blank=True)
+    phone = models.CharField(max_length=16, null=True, blank=True)
+    address = models.CharField(max_length=256, null=True, blank=True)
+    language = models.CharField(max_length=2, choices=LANG, default="en", null=True, blank=True)
+
+
+class EndUserProfile(UserProfileBase):
+    MARITAL_STATUS = (
+        ("single", "single"),
+        ("married", "married"),
+        ("divorced", "divorced"),
+        ("widowed", "widowed")
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='enduser_profile')
+
+    marital_status = models.CharField(max_length=16, choices=MARITAL_STATUS, default='married', null=True, blank=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    height = models.PositiveIntegerField(null=True, blank=True)
+
     def __str__(self):
-        return "[" + self.account_role + "]" + self.first_name + " " + self.last_name + "; " + "email: " + self.email
+        return "[" + self.account_role + "]" + self.user.first_name + " " + self.user.last_name + "; " + "email: " + self.user.email
 
     __unicode__ = __str__
 
 
-class Caregiver(UserAccount):
-    caretaker = models.ForeignKey(UserAccount, related_name="caregivers")
+class CaregiverProfile(UserProfileBase):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='caregiver_profile')
+    caretaker = models.ForeignKey(User, related_name="caregivers")
 
 
-class HealthProfessional(UserAccount):
+class HealthProfessionalProfile(UserProfileBase):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='doctor_profile')
+
     title = models.CharField(max_length=32)
     affiliation = models.CharField(max_length=128)
     specialty = models.CharField(max_length=64)
-    patients = models.ManyToManyField(UserAccount, related_name="doctors")
 
+    patients = models.ManyToManyField(User, related_name="doctors")
 
-# We skip defining a model for the UserSession, because this will most likely be in the form of a Django Middleware or
-# REST application (e.g. Django-REST-Framework)
-# class UserSession(models.Model):
-#     pass
 
 # ================ Devices ================
 class Device(models.Model):
@@ -102,9 +100,9 @@ class Device(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now = True)
 
-    activation_date = models.DateTimeField(default = timezone.now())
+    activation_date = models.DateTimeField(default = timezone.now)
 
-    used_by = models.ManyToManyField(UserAccount, related_name="used_devices", through="DeviceUsage")
+    used_by = models.ManyToManyField(User, related_name="used_devices", through="DeviceUsage")
 
     def __str__(self):
         users = self.used_by.all()
@@ -114,7 +112,7 @@ class Device(models.Model):
 
 
 class DeviceUsage(models.Model):
-    user_account = models.ForeignKey(UserAccount)
+    user = models.ForeignKey(User)
     device = models.ForeignKey(Device)
     uses_since = models.DateField(auto_now=True)
     access_info = JSONField()
@@ -130,7 +128,7 @@ class DeviceUsage(models.Model):
 
 
 class ExternalMonitoringService(models.Model):
-    user = models.ForeignKey(UserAccount, related_name="used_monitoring_services")
+    user = models.ForeignKey(User, related_name="used_monitoring_services")
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -177,7 +175,7 @@ class Measurement(models.Model):
     precision = models.PositiveIntegerField(default=100, null = True, blank=True,
                                             validators=[validate_precision_range])
     value_info = JSONField()
-    user = models.ForeignKey(UserAccount, related_name="health_measurements")
+    user = models.ForeignKey(User, related_name="health_measurements")
     device = models.ForeignKey(Device, related_name="performed_measurements")
 
     context_info = JSONField(null=True, blank = True)
@@ -191,6 +189,9 @@ class Measurement(models.Model):
 
 # ================ User Activity Information ================
 class Activity(models.Model):
+    class Meta:
+        verbose_name_plural = "activities"
+
     ACTIVITY_TYPE = (
         ("personal", "personal"),
         ("exercise", "exercise"),
@@ -207,7 +208,7 @@ class Activity(models.Model):
 
     # id = models.AutoField(primary_key=True)
     backend_id = models.CharField(unique=True, max_length=64, null=True, blank = True)
-    user = models.ForeignKey(UserAccount)
+    user = models.ForeignKey(User)
 
     name = models.CharField(max_length=64, null=True, blank=True)
     activity_type = models.CharField(max_length=16, choices=ACTIVITY_TYPE, default="personal")
