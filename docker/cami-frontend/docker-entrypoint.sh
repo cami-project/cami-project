4ee652567986649c121d2b0a956bc8e47a1196a6
Alexandru Sorici <alex.sorici@gmail.com>
2017-03-10 20:32:00 +0200

    Adding root cami-project folder to PYTHONPATH to make CAMI service modules accessible from one another

--- docker/cami-frontend/docker-entrypoint.sh
+++ docker/cami-frontend/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Export root of cami-project to PYTHONPATH, to make all services accessible as modules
+export PYTHONPATH="${PYTHONPATH}:/cami-project"
+
 # Using hardcoded values for paths for now.
 # TODO(@iprunache): find a way to make the paths configurable
 
