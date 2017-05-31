039500e901a345281f17f98a734bd1e24e6a11c7
Hamza Sabljakovic <Hamza@Hamzas-MacBook-Pro.local>
2017-05-31 16:27:34 +0200

    #196 Console input interface for creating custom mockup data

--- DSS/DSS.Main/Program.cs
+++ DSS/DSS.Main/Program.cs
@@ -43,7 +43,9 @@ namespace DSS.Main
             {
 				Console.WriteLine("=========================");
                 Console.WriteLine("[1] - Generate default fall event {\"Name\":\"Fall\",\"Type\":\"Fall\",\"Val\":\"High\"}");
-                Console.WriteLine("ctrl + c - Exit");
+				Console.WriteLine("[2] - Generate fall event with custom data");
+
+				Console.WriteLine("ctrl + c - Exit");
 				Console.WriteLine("=========================");
 
                 var num = Console.ReadLine();
@@ -52,9 +54,24 @@ namespace DSS.Main
                     rmq.Write(new Event("Fall", "Fall", "High").ToJson());
 
                 if (num == "2")
-                    return;
+                {
+                    var input = new string [3];
+                    Console.Write("Name: ");
+                    input[0] = Console.ReadLine();
 
-                run = num != "2";
+					Console.Write("Type: ");
+					input[1] = Console.ReadLine();
+
+					Console.Write("Value: ");
+					input[2] = Console.ReadLine();
+
+                    rmq.Write(new Event(input[0], input[1], input[2]).ToJson());
+
+				}
+
+
+
+                run = num != "3";
 			}
             rmq.Dispose();
 
