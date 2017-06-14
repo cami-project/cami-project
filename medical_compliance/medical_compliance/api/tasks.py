25d6ec98a8648df101196a02b36bd2256b8cff76
Alexandru Cretu <cretu.alexandru007@gmail.com>
2017-06-14 16:54:45 +0300

    #250 Re-add timezone parameter that was wrongly removed

--- medical_compliance/medical_compliance/api/tasks.py
+++ medical_compliance/medical_compliance/api/tasks.py
@@ -40,7 +40,7 @@ app.conf.update(
 
 @app.task(name='medical_compliance_measurements.process_weight_measurement')
 def process_weight_measurement(cami_user_id, device_id,
-                               measurement_type, measurement_unit, timestamp, value):
+                               measurement_type, measurement_unit, timestamp, timezone, value):
     logger.debug("[medical-compliance] Process weight measurement: %s" % (locals()))
 
     endpoint_uri = store_utils.STORE_ENDPOINT_URI
