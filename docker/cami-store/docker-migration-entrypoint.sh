85fa8b3e7c74593262e54138d827661503428170
Alexandru Sorici <alex.sorici@gmail.com>
2017-06-28 14:49:32 +0300

    #264 Adding fixture file for CAMI Store bootstrapping.

--- docker/cami-store/docker-migration-entrypoint.sh
+++ docker/cami-store/docker-migration-entrypoint.sh
@@ -7,23 +7,23 @@ python /cami-project/store/manage.py migrate
 python /cami-project/store/manage.py loaddata initialize.yaml
 
 # Create Django superuser account if not exists
-USER="cami"
-PASS="CamiAdmin4321"
-MAIL="proiect.cami@gmail.com"
-script="
-from django.contrib.auth.models import User;
-
-username = '$USER';
-password = '$PASS';
-email = '$MAIL';
-
-if User.objects.filter(username=username).count()==0:
-    User.objects.create_superuser(username, email, password);
-    print('Superuser created.');
-else:
-    print('Superuser creation skipped.');
-"
-echo "$script" | python /cami-project/store/manage.py shell
-
-# Run bootstrapping script
-python /cami-project/store/bootstrap_user_info.py
+#USER="cami"
+#PASS="CamiAdmin4321"
+#MAIL="proiect.cami@gmail.com"
+#script="
+#from django.contrib.auth.models import User;
+#
+#username = '$USER';
+#password = '$PASS';
+#email = '$MAIL';
+#
+#if User.objects.filter(username=username).count()==0:
+#    User.objects.create_superuser(username, email, password);
+#    print('Superuser created.');
+#else:
+#    print('Superuser creation skipped.');
+#"
+#echo "$script" | python /cami-project/store/manage.py shell
+#
+## Run bootstrapping script
+#python /cami-project/store/bootstrap_user_info.py
