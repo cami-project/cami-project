e052966f3ea8d3c7f3fb044e2454ad16a674fb07
Hamza Sabljakovic <Hamza@Hamzas-MacBook-Pro.local>
2017-06-29 08:33:25 +0200

    #261 In memory temporary database

---
+++ DSS/DSS.FuzzyInference.Playground/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DSS.Delegate;
+
+namespace DSS.FuzzyInference.Playground
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            var timerQueue = new TimerQueue<Event>();
+
+
+            timerQueue.Push(new Event("event 1","type 1", "1", "device"), -1 );
+			timerQueue.Push(new Event("event 2", "type 1", "1", "device"), 1);
+			timerQueue.Push(new Event("event 3", "type 1", "1", "device"), -2);
+			timerQueue.Push(new Event("event 4", "type 1", "1", "device"), -5);
+			timerQueue.Push(new Event("event 5", "type 1", "1", "device"), 1);
+
+
+            //every time you recieve a new event first refreshh
+            timerQueue.Refresh();
+
+            foreach (var item in timerQueue.Queue)
+            {
+                Console.WriteLine(item.ToString());
+			}
+
+        }
+    }
+}
--- DSS/DSS.FuzzyInference/FuzzyHandler.cs
+++ DSS/DSS.FuzzyInference/FuzzyHandler.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using DSS.Delegate;
 
+
 namespace DSS.FuzzyInference
 {
     public class FuzzyHandler : IRouterHandler<Event>
     {
-        public string Name => "FUZZY-INFERENCE";
+        public TimerQueue<Event> Queue { get; set; }
+
+	    public string Name => "FUZZY-INFERENCE";
 
         public void Handle(Event obj)
         {
 
+            Queue.Refresh();
+
             Console.WriteLine("OVO je fuzzy inference");
+
+
+            Queue.Push(obj, 1);
 
         }
     }
