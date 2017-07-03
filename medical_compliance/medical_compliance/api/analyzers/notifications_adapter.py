b7cbd5a435b66e643e96082944933747f1a5d401
Alexandru Cretu <cretu.alexandru007@gmail.com>
2017-07-03 13:12:33 +0300

    #258 Update the notification_adapter to use the new fronend and store setup

--- medical_compliance/medical_compliance/api/analyzers/notifications_adapter.py
+++ medical_compliance/medical_compliance/api/analyzers/notifications_adapter.py
@@ -1,6 +1,8 @@
 import celery
 from celery import Celery
 
+from medical_compliance.api import store_utils
+
 class NotificationsAdapter():
     __celery_notification_task = 'frontend.send_notification'
     __celery_queue = 'frontend_notifications'
@@ -21,6 +23,17 @@ class NotificationsAdapter():
             queue=self.__celery_queue
         )
 
+        store_utils.insert_journal_entry(
+            user="/api/v1/user/%d/" % 3,
+            type=notification_type,
+            severity=severity,
+            timestamp=timestamp,
+            message=message,
+            description=description,
+            # This is dummy, will be removed anyway when refactoring medical_compliance
+            measurement="/api/v1/measurement/%d/" % 1
+        )
+
     def send_elderly_notification(self, user_id, notification_type, severity, message, description, timestamp = None):
         celery_app = self.__get_celery_app()
         celery_app.send_task(
@@ -30,4 +43,15 @@ class NotificationsAdapter():
                 message
             ),
             queue=self.__celery_queue
+        )
+
+        store_utils.insert_journal_entry(
+            user="/api/v1/user/%d/" % 2,
+            type=notification_type,
+            severity=severity,
+            timestamp=timestamp,
+            message=message,
+            description=description,
+            # This is dummy, will be removed anyway when refactoring medical_compliance
+            measurement="/api/v1/measurement/%d/" % 1
         )
