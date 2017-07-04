9610bf979571073201fa0d83cf0f87de15d720f7
Alexandru Cretu <cretu.alexandru007@gmail.com>
2017-07-05 02:37:51 +0300

    #254 Remove DB config

--- insertion/insertion/settings.py
+++ insertion/insertion/settings.py
@@ -156,12 +156,7 @@ SENTRY_AUTO_LOG_STACKS = True
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
 
 
 # Password validation
