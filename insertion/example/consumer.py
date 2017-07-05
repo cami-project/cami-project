f140de32d5c685f52f8902f1546b3364f4336f18
Alexandru Cretu <cretu.alexandru007@gmail.com>
2017-07-05 14:19:59 +0300

    #254 Add an example to see how this works

---
+++ insertion/example/consumer.py
@@ -0,0 +1,34 @@
+import pika
+
+parameters = pika.URLParameters('amqp://cami:cami@127.0.0.1:5673/cami')
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+# Create a temporary queue only for receiving data from the exchange
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(
+	# We want to get 'measurement' data, so we bind with
+	# the 'measurements' exchange
+    exchange='measurements',
+    queue=queue_name,
+    # The 'measurements' exhange is of `topic` type
+    # We can get the messages using wildcards
+    # Using this routing key, we'll basically receive all
+    # the measurements, no matter their type
+    routing_key='measurement.*'
+)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+def callback(ch, method, properties, body):
+    print(" [x] %r" % body)
+
+channel.basic_consume(
+    callback,
+    queue=queue_name,
+    no_ack=True
+)
+
+channel.start_consuming()
