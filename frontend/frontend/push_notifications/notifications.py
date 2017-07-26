4f667b3945cf1d55e3dc3392add28fc8be5c1eba
Alexandru Cretu <cretu.alexandru007@gmail.com>
2017-07-26 14:36:33 +0300

    #269 Handle push notifications exceptions

--- frontend/frontend/push_notifications/notifications.py
+++ frontend/frontend/push_notifications/notifications.py
@@ -10,16 +10,28 @@ logger = logging.getLogger("frontend.push_notifications.notifications")
 def send_message(devices, message, **kwargs):
     for device in devices:
         if device['type'] == "APNS":
-            apns.apns_send_message(
-                device['registration_id'],
-                message,
-                None,
-                None,
-                **kwargs
-            )
-            logger.debug("[frontend] Sent notification to device (%s): \"%s\"" % (
-                str(device),
-                message
-            ))
+            try:
+                apns.apns_send_message(
+                    device['registration_id'],
+                    message,
+                    None,
+                    None,
+                    **kwargs
+                )
+
+                logger.debug(
+                    "[frontend] Sent notification to device (%s): \"%s\"",
+                    str(device),
+                    message
+                )
+            except apns.APNSError as e:
+                logger.debug(
+                    "[frontend] Failed sending notification to device (%s): %s",
+                    str(device),
+                    repr(e)
+                )
         else:
-            logger.debug("[frontend] Failed sending notification to device (%s)." % str(device))
+            logger.debug(
+                    "[frontend] Failed sending notification to device (%s): push notification provider currently not supported",
+                    str(device)
+                )
