e1691caa8a01331ab0a0929591f9cdee30f1bc9e
rtud <radu@rtud.ro>
2017-07-26 16:20:51 +0300

    #259 - Change API endpoints used for Journal & Push Notifications

--- application/env.example.js
+++ application/env.example.js
@@ -6,11 +6,11 @@ module.exports = {
   AUTH0_CLIENT_ID: 'xqN8fSjHL6850gI2m8AUkk5GICgvTCUs',
   AUTH0_DOMAIN: 'vitamin.eu.auth0.com',
   POLL_INTERVAL_MILLIS: 5000,
-  NOTIFICATIONS_REST_API: 'http://cami.vitaminsoftware.com:8001/api/v1/notifications/',
+  NOTIFICATIONS_REST_API: 'http://cami.vitaminsoftware.com:8008/api/v1/journal_entries/',
   ACTIVITIES_LAST_EVENTS: 'http://cami.vitaminsoftware.com:8008/api/v1/activity/last_activities/',
   WEIGHT_MEASUREMENTS_LAST_VALUES: 'http://cami.vitaminsoftware.com:8000/api/v1/weight-measurements/last_values/',
   HEARTRATE_MEASUREMENTS_LAST_VALUES: 'http://cami.vitaminsoftware.com:8000/api/v1/heartrate-measurements/last_values/',
   STEPS_MEASUREMENTS_LAST_VALUES: 'http://cami.vitaminsoftware.com:8000/api/v1/steps-measurements/last_values/',
   NOTIFICATIONS_SUBSCRIPTION_API: 'http://cami.vitaminsoftware.com:8000/subscribe_notifications/',
-  MOBILE_NOTIFICATION_KEY_API: 'http://cami.vitaminsoftware.com:8001/api/v1/mobile-notification-key/'
+  MOBILE_NOTIFICATION_KEY_API: 'http://cami.vitaminsoftware.com:8001/api/v1/push-notification-subscribe/'
 };
