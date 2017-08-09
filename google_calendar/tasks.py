43f056e1de46b7abf1c70c4601ddbad15b761273
Alexandru Cretu <cretu.alexandru007@gmail.com>
2017-08-09 21:11:12 +0300

    #275 Create task for sending current minute reminders. Schedule the task to be called every minute.

--- google_calendar/tasks.py
+++ google_calendar/tasks.py
@@ -1,4 +1,6 @@
+import time
 import logging
+import datetime
 
 from kombu import Queue, Exchange
 from celery import Celery
@@ -26,3 +28,19 @@ def sync_activities():
 @app.task(name='google_calendar.sync_activities_for_user')
 def sync_activities_for_user(user):
     activities.sync_for_user(user)
+
+@app.task(name='google_calendar.process_reminders')
+def process_reminders():
+    # Get current minute in UTC timestamp
+    time_now = datetime.datetime.utcnow().strftime("%a, %d %b %Y %H:%M")
+    time_now = time.mktime(time.strptime(time_now, "%a, %d %b %Y %H:%M"))
+    
+    # Get all the activities that should be reminded in the current minute
+    # and send reminders for each one of them
+    activities = store_utils.activity_get(reminders__contains=int(time_now))
+    for activity in activities:
+        app.send_task('google_calendar.send_reminder', [activity])
+
+@app.task(name='google_calendar.send_reminder')
+def send_reminder(activity):
+    pass
