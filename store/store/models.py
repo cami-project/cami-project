cd293f1ec67f356a78cd1002bb384416b73a4199
Alex Sorici <alex.sorici@gmail.com>
2017-08-18 16:01:20 +0300

    #290 Adding gateway_id field in Measurements model and updating API resources and documentation accordingly.

--- store/store/models.py
+++ store/store/models.py
@@ -170,6 +170,9 @@ class Measurement(models.Model):
     )
     value_info = JSONField()
     ok = models.BooleanField()
+
+    gateway_id = models.CharField(max_length=64, null=True, blank=True)
+
     
     def __str__(self):
         return (
