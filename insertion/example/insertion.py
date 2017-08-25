e6e95867e8acbca85cf92da9e2967a417eb7a427
Alexandru Cretu <cretu.alexandru007@gmail.com>
2017-08-25 13:39:52 +0300

    #285 Update insertion code to use the new Gateway model

--- insertion/example/insertion.py
+++ insertion/example/insertion.py
@@ -9,7 +9,7 @@ data = {
 	"user": "/api/v1/user/14/",
 	"device": "/api/v1/device/1/",
 	"value_info": "{'systolic': 115, 'diastolic': 70}",
-	"gateway_id": "9f508c55-dfd2-4ea6-8228-af991c123f17"
+	"gateway": "/api/v1/gateway/1/"
 }
 
 response = requests.post(
