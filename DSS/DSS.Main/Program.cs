77aa47cd25f873cf05b56a7c0cbb0506c336d369
sabljakovich <hamza.sabljakovic@edu.fit.ba>
2017-11-03 10:06:13 +0100

    #401 motion handler bug fix

--- DSS/DSS.Main/Program.cs
+++ DSS/DSS.Main/Program.cs
@@ -45,14 +45,15 @@ namespace DSS.Main
 				new MeasurementHandler(),
                 new FallHandler(),
                 new SensorToLocationHandler(),
-                new ReminderHandler()
+                new ReminderHandler(),
+                new MotionEventHandler()
 			};
 
             var url = "amqp://cami:cami@cami-rabbitmq:5672/cami";
             try
             {
                 //var rmqEvents = new RmqExchange(url, "events", "event.*", (json) => { handlers[0].Handle(json); handlers[2].Handle(json); handlers[3].Handle(json); } );
-                var rmqEvents = new RmqExchange(url, "events", "event.*", (json) => { handlers[0].Handle(json); handlers[4].Handle(json);  } );
+                var rmqEvents = new RmqExchange(url, "events", "event.*", (json) => { handlers[0].Handle(json); handlers[4].Handle(json); handlers[5].Handle(json);  } );
                 var rmqMeasurements = new RmqExchange(url, "measurements", "measurement.*", (json) => { handlers[1].Handle(json);  });
 
 			}
