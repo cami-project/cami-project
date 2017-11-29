ba7b3cfbd582a4877d2b7d59507f706b948f55a3
rtud <radu@rtud.ro>
2017-11-29 12:45:04 +0200

    #430 - Triggering a one time elder app data fetch whe app is coming back from background

--- application/src/modules/AppView.js
+++ application/src/modules/AppView.js
@@ -1,5 +1,5 @@
 import React, {PropTypes} from 'react';
-import {View, StyleSheet, ActivityIndicator, Alert} from 'react-native';
+import {View, StyleSheet, ActivityIndicator, Alert, AppState} from 'react-native';
 import NavigationViewContainer from './navigation/NavigationViewContainer';
 import AppRouter from './AppRouter';
 import * as auth0 from '../services/auth0';
@@ -9,6 +9,7 @@ import store from '../redux/store';
 import DeveloperMenu from '../components/DeveloperMenu';
 import PushNotification from 'react-native-push-notification';
 import * as PushNotificationsState from './push-notifications/PushNotificationsState';
+import * as HomepageStateActions from './homepage/HomepageState';
 
 const AppView = React.createClass({
   propTypes: {
@@ -45,11 +46,38 @@ const AppView = React.createClass({
   },
 
   componentWillReceiveProps({isReady, isLoggedIn}) {
+    console.log('[AppView] - this.props.isReady: ' + this.props.isReady + ', isReady: ' + isReady + ', this.props.isLoggedIn: ' + this.props.isLoggedIn + ', isLoggedIn: ' + isLoggedIn);
+
     if (!this.props.isReady) {
       if (isReady && !isLoggedIn) {
         auth0.showLogin();
       }
     }
+  },
+
+  componentWillMount() {
+    AppState.addEventListener('change', (state) => {
+      if (state === 'active') {
+        console.log('[AppView] - Application state is now [active].');
+
+        var loggedIn = store.getState().get('auth').get('isLoggedIn');
+
+        if (this.props.isReady && loggedIn) {
+
+          var userType = store.getState().get('auth').get('currentUser').get('userMetadata').get('userType');
+          console.log('[AppView] - [' + userType  + '] type user is logged in and app is ready.');
+
+          if (userType === 'elderly') {
+            store.dispatch(HomepageStateActions.requestOneTimeNotification());
+            console.log('[AppView] - Triggered a one time force fetch of [elder] data.');
+          }
+        }
+      }
+
+      if(state === 'background'){
+        console.log('[AppView] - Application state is now [background].');
+      }
+    });
   },
 
   render() {
