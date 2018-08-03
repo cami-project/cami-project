2d4df03b42e6e35be6902471ad3a36c6a0ebc156
JohnPrime <bogdan_ionut.ion@cti.pub.ro>
2018-08-03 18:45:09 +0300

    #461 Remove busy celery.pid before starting celery worker.

--- docker/google_calendar/docker-task-scheduler-entrypoint.sh
+++ docker/google_calendar/docker-task-scheduler-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Remove busy pid
+rm -f ./google_calendar/celerybeat.pid
+
 # Export root of cami-project to PYTHONPATH, to make all services accessible as modules
 export PYTHONPATH="${PYTHONPATH}:/cami-project"
 
