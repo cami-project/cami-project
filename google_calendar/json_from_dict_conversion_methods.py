f689deadf9259ebdf91351c8ee1f1fc65ba4226d
JohnPrime <bogdan_ionut.ion@cti.pub.ro>
2018-08-20 18:17:57 +0300

    #460 Fix activity period dict creation. Remove ids from all dict creation methods as they are unnecessary.

--- google_calendar/json_from_dict_conversion_methods.py
+++ google_calendar/json_from_dict_conversion_methods.py
@@ -1,6 +1,3 @@
-import json
-from datetime import datetime
-
 # difficulty types
 EASY, MEDIUM, HARD = "EASY", "MEDIUM", "HARD"
 
@@ -9,9 +6,9 @@ def create_time_dict(time_name, hour, minutes):
     return time_name, {"hour": hour, "minutes": minutes}
 
 
-def create_period_dict(period_name, hour, minutes, day_index):
-    return {period_name: {"time": {"hour": hour, "minutes": minutes},
-                          "weekDay": {"dayIndex": day_index}, "periodIndex": 0}}
+def create_period_dict(hour, minutes, day_index):
+    return {"time": {"hour": hour, "minutes": minutes},
+            "weekDay": {"dayIndex": day_index}, "periodIndex": 0}
 
 
 def create_excluded_time_periods_penalty_dict():
@@ -53,12 +50,11 @@ def create_activity_category_dict(code, domain=None):
     return activity_category_dict
 
 
-def create_activity_type_dict(id, code, duration, instances_per_day=0, instances_per_week=0,
+def create_activity_type_dict(code, duration, instances_per_day=0, instances_per_week=0,
                               calories=0, description="", activity_category=None, difficulty=None, imposed_period=None,
                               permitted_intervals=None):
     """
     Stores all the information about an activity.
-    :param id: serialization id
     :param code: name of the activity
     :param duration: duration of the activity
     :param instances_per_day: instances of the activity in a day, repeating every day
@@ -72,7 +68,7 @@ def create_activity_type_dict(id, code, duration, instances_per_day=0, instances
     :return: a dictionary with all the given information about the activity
     """
     # activity code must be set nevertheless, even if it can miss from dict
-    activity_type_dict = {"id": id, "code": code, "duration": duration, "calories": calories,
+    activity_type_dict = {"code": code, "duration": duration, "calories": calories,
                           "instancesPerDay": instances_per_day, "instancesPerWeek": instances_per_week}
 
     if imposed_period:
@@ -103,8 +99,8 @@ def create_activity_dict(activity_type_dict, uuid, activity_sub_class_type="Norm
     return activity_dict
 
 
-def create_new_activity_dict(id, activity=None, excluded_time_periods_penalty=None, relative_activity_penalty=None):
-    new_activity_dict = {"id": id}
+def create_new_activity_dict(activity=None, excluded_time_periods_penalty=None, relative_activity_penalty=None):
+    new_activity_dict = {}
 
     if activity:
         new_activity_dict["activity"] = activity
@@ -127,8 +123,8 @@ def create_new_activities_dict(new_activities_list):
     return new_activities_dict
 
 
-def create_deleted_activity_dict(id, name, uuid):
-    deleted_activity_dict = {"id": id, "name": name, "uuid": uuid}
+def create_deleted_activity_dict(name, uuid):
+    deleted_activity_dict = {"name": name, "uuid": uuid}
     return deleted_activity_dict
 
 
@@ -139,36 +135,32 @@ def create_deleted_activities_dict(deleted_activities_list):
     return deleted_activities_dict
 
 
-def get_datetime_from_timestamp(timestamp):
-    return datetime.fromtimestamp(timestamp / 1000.0)
-
-
-def generate_json_from_dict(my_dict):
-    json_str = json.dumps(my_dict, separators=(',', ':'))
-    return json_str
-
-
 def main():
-    # print(create_activity_period_dict(12, 30, 5))
-    # print(create_activity_dict(create_activity_period_dict(12, 30, 5)))
+    # testing purposes
     d = create_new_activities_dict(
-        [create_new_activity_dict(0, create_activity_dict(create_activity_type_dict(id=0, code="Yoga", duration=30,
-                                                                                    activity_category=create_activity_category_dict(
-                                                                                        code="Indoor physical exercises",
-                                                                                        domain=create_activity_domain_dict(
-                                                                                            code="Health Related Activities"))),
-                                                          uuid="28204e9fcf45459baaa9aa348ded622a")),
-         create_new_activity_dict(1, create_activity_dict(create_activity_type_dict(id=1, code="FootBall", duration=30,
-                                                                                    activity_category=create_activity_category_dict(
-                                                                                        code="Outdoor physical exercises",
-                                                                                        domain=create_activity_domain_dict(
-                                                                                            code="Leisure Activities"))),
-                                                          uuid="6b05c3ff6cd1449489daae351d73a079"))
-         ])
-    print generate_json_from_dict(d)
-    print get_datetime_from_timestamp(1531832400000)
-    print generate_json_from_dict(create_deleted_activities_dict(
-        [create_deleted_activity_dict(0, "Yoga", "f323c5807f8748e1b057d1b56de70f0a")]))
+        new_activities_list=
+        [create_new_activity_dict(activity=create_activity_dict(
+            activity_type_dict=create_activity_type_dict(
+                code="Yoga", duration=30,
+                activity_category=create_activity_category_dict(
+                    code="Indoor physical exercises",
+                    domain=create_activity_domain_dict(
+                        code="Health Related Activities"))),
+            uuid="28204e9fcf45459baaa9aa348ded622a")),
+            create_new_activity_dict(
+                activity=create_activity_dict(
+                    activity_type_dict=create_activity_type_dict(
+                        code="Football", duration=30,
+                        activity_category=create_activity_category_dict(
+                            code="Outdoor physical exercises",
+                            domain=create_activity_domain_dict(
+                                code="Leisure Activities"))),
+                    uuid="6b05c3ff6cd1449489daae351d73a079"))
+        ])
+
+    print d
+    print create_deleted_activities_dict(
+        [create_deleted_activity_dict(name="Yoga", uuid="f323c5807f8748e1b057d1b56de70f0a")])
 
 
 if __name__ == '__main__':
